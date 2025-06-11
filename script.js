172a6f77570e106fd87259c3988251c9af4e9576
Selva-C <postbox221@outlook.com>
2025-06-11 15:11:35 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-document.querySelector("button").addEventListener("click",foo);
-document.addEventListener("keydown", function(event) { 
+document.querySelector("button").addEventListener("click", foo);
+document.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         foo();
     }
 });
 
-async function foo(){
+async function foo() {
     try {
         let result = document.getElementById("Text").value;
         let data = await fetch(`https://api.tvmaze.com/search/shows?q=${result}`);
         let data1 = await data.json();
-        console.log(data1)
         let div = document.getElementById("collable");
-        div.innerHTML=`<div class="card" style="width: 18rem;">
+        div.innerHTML = `<div class="card" style="width: 18rem;">
   <img src="${data1[0].show.image.medium}" class="card-img-top" alt="image">
   <div class="card-body">
   <h3 class="card-tittle">${data1[0].show.name}</h3>
